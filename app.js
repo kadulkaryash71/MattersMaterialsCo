c2f3d8612a994966d47baa4e727bc055c6586989
Yash Kadulkar <yashkadulkar@gmail.com>
2022-09-06 21:36:17 +0530

    Replaced placeholders

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hey there, Matty! I noticed you lurking through the hole. I would like to get to know you better yo. Head over to the \"About\" Page, that'll fire up our conversation.";
+const aboutContent = "Matters & Materials Co. will help you through your journey of logistics and networking in the ever evolving market by keeping you up-to-date. Enjoy finding people like yourself express their opinions on a variety of modules relating to the business. But remember, the scope of a topic can vary from an elephant to as small as an atom. Let's pause the chitchat and whispers for a while and dive in this new little tippy-tappy world of yours. We can't wait to have you on board!";
+const contactContent = "You here to contact me? 😳👉👈\nSure do. Why don't you drop a new post! I will see you there.";
 
 const app = express();
 
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 
 mongoose.connect("mongodb+srv://admin:mongodb123@cluster0.hxtxool.mongodb.net/blogger", { useNewUrlParser: true });
 
@@ -26,14 +27,6 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema)
 
-// test item for insertion
-// const blog = {
-//     _id: "day-1",
-//     title: "Day 1",
-//     post: "Ipsum aute fugiat esse aute ad irure Lorem est id aliquip."
-// }
-// const newBlog = new Blog(blog);
-// newBlog.save();
 
 app.get("/", (req, res) => {
     Blog.find({}, (e, docs) => {
